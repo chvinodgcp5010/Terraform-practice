a836ceddad09b1987e100fafdd159f1fd65d3649
chvinodgcp5010 <106317380+chvinodgcp5010@users.noreply.github.com>
2022-10-13 16:43:25 +0530

    Update s3-backend-remotestate.tf

--- s3-backend-remotestate.tf
+++ s3-backend-remotestate.tf
@@ -1,8 +1,10 @@
+#https://www.terraform.io/language/settings/backends/s3
+
 terraform {
   backend "s3" {
     bucket = "kplabs-terraform-vnd"
-    dynamodb_table = "my-dynamodb"
-    key    = "vnd.tfstate"
+    dynamodb_table = "my-dynamodb"          #create dynamodb table manually
+    key    = "vnd.tfstate"                  #it saves tfstate on provided bucket and store with a name 
     region = "us-east-1"
   }
 }
