c64f4a2898ee91fb822d47d20f8e17642a2a4540
chvinodgcp5010 <106317380+chvinodgcp5010@users.noreply.github.com>
2022-10-13 16:41:37 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -6,10 +6,16 @@ data "aws_ami" "ubuntu" {
     values = ["amzn2-ami-kernel-5.10-hvm-2.0.20220912.1-x86_64-gp2*"]
   }
 }
+
 resource "aws_instance" "web" {
   ami               = data.aws_ami.ubuntu.id
   instance_type     = var.instance_type-vnd
-  availability_zone = "${var.region-vnd}a"
+  #availability_zone = "${var.region-vnd}a"
+  #https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/instance
+  network_interface {
+    network_interface_id = aws_network_interface.nic.id
+    device_index         = 0
+  }
 
   tags = {
     Name = var.name-vnd
