b612a86a8499689b51bbb3f5247af21ff4fb68fc
chvinodgcp5010 <106317380+chvinodgcp5010@users.noreply.github.com>
2022-10-13 16:43:46 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,5 +1,7 @@
 #https://www.terraform.io/language/values/variables
 
+#https://www.terraform.io/language/values/variables
+
 variable "instance_type-vnd" {
   description = "type of EC2 instance to provision."
   default     = "t2.micro"
@@ -13,4 +15,29 @@ variable "region-vnd" {
 variable "name-vnd" {
   description = "name to pass to Name tag"
   default     = "terraform-ec2-vnd"
+}
+
+variable "vpc-name" {
+  description = "name to pass to Name tag"
+  default     = "terraform-ec2-vpc"
+}
+
+variable "subnet-name" {
+  description = "name to pass to Name tag"
+  default     = "terraform-ec2-subnet"
+}
+
+variable "nic-name" {
+  description = "name to pass to Name tag"
+  default     = "terraform-ec2-nic"
+}
+
+variable "igw-name" {
+  description = "name to pass to Name tag"
+  default     = "terraform-ec2-igw"
+}
+
+variable "rt" {
+  description = "name to pass to Name tag"
+  default     = "terraform-ec2-rt"
 }
