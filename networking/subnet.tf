2902c373d28b2068af8250425fbcf5e396d58383
chvinodgcp5010 <106317380+chvinodgcp5010@users.noreply.github.com>
2022-10-14 16:13:22 +0530

    Update subnet.tf

--- networking/subnet.tf
+++ networking/subnet.tf
@@ -1,6 +1,6 @@
 resource "aws_subnet" "my_subnet" {
   vpc_id            = aws_vpc.my_vpc.id
-  cidr_block        = "172.16.10.0/24"
+  cidr_block        = "10.0.1.0/24                        #"172.16.10.0/24"
   #availability_zone = "us-east-1a"
   map_public_ip_on_launch = "true"
   #https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/subnet
