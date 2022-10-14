b1ea0a36689d0fe46a0b0dac414704e089c4cb1d
chvinodgcp5010 <106317380+chvinodgcp5010@users.noreply.github.com>
2022-10-14 14:42:43 +0530

    Create vpc.tf

---
+++ networking/vpc.tf
@@ -0,0 +1,9 @@
+#https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/instance#network_interface_id
+
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "172.16.0.0/16"
+
+  tags = {
+    Name = var.vpc-name
+  }
+}
