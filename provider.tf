f691e12d6a2c776ec539fb1cfcac3c5e464019d1
chvinodgcp5010 <106317380+chvinodgcp5010@users.noreply.github.com>
2022-10-14 11:00:46 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,14 @@
 # Any selected backend applies to the entire configuration, so Terraform expects provider configurations only in the root module.so we need to add
 # provider.tf on root folder here.
 
+required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.74.0"
+    }
+  }
+}
+
 terraform {
   required_version = "> 0.12.0"
 }
