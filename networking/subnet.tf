e7fbacc57e904c13b0ab6b31ff0098cdd123a6f2
chvinodgcp5010 <106317380+chvinodgcp5010@users.noreply.github.com>
2022-10-14 14:43:23 +0530

    Create subnet.tf

---
+++ networking/subnet.tf
@@ -0,0 +1,13 @@
+resource "aws_subnet" "my_subnet" {
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = "172.16.10.0/24"
+  #availability_zone = "us-east-1a"
+  map_public_ip_on_launch = "true"
+  #https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/subnet
+  #true to indicate that instances launched into the subnet should be assigned a public IP address. Default is false.
+  tags = {
+    Name = var.subnet-name
+  }
+}
+
+
