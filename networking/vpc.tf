8d21e4c6edba60e91d23369b0a44aa51b496f0f0
chvinodgcp5010 <106317380+chvinodgcp5010@users.noreply.github.com>
2022-10-14 16:13:43 +0530

    Update vpc.tf

--- networking/vpc.tf
+++ networking/vpc.tf
@@ -1,7 +1,7 @@
 #https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/instance#network_interface_id
 
 resource "aws_vpc" "my_vpc" {
-  cidr_block = "172.16.0.0/16"            # "10.0.0.0/16"         
+  cidr_block = "10.0.0.0/16"                   #"172.16.0.0/16"                    
  
   tags = {
     Name = var.vpc-name
