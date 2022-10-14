892559c887e50fe6f6a86d417eaabd5fc5c2c083
chvinodgcp5010 <106317380+chvinodgcp5010@users.noreply.github.com>
2022-10-14 16:22:11 +0530

    Update subnet.tf

--- networking/subnet.tf
+++ networking/subnet.tf
@@ -6,8 +6,18 @@ resource "aws_subnet" "my_subnet" {
   #https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/subnet
   #true to indicate that instances launched into the subnet should be assigned a public IP address. Default is false.
   tags = {
-    Name = var.subnet-name
+    Name = var.subnet1-name
   }
 }
 
-
+resource "aws_subnet" "my_subnet" {
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = "10.0.2.0/24                        #"172.16.10.0/24"
+  #availability_zone = "us-east-1a"
+  map_public_ip_on_launch = "true"
+  #https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/subnet
+  #true to indicate that instances launched into the subnet should be assigned a public IP address. Default is false.
+  tags = {
+    Name = var.subnet2-name
+  }
+}
